f082178e79cacdce161eb931935efc8fe4fcc1e0
arif rahman <matrep88@hotmail.com>
2017-05-21 22:26:02 +0800

    'add-typed-animation'

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -9,6 +9,12 @@ $( document ).ready(function(){
   setTimeout(function(){
     $('.fly-in-text').removeClass('hidden')
   }, 300)
+  // typed animation
+  Typed.new('#typed', {
+			stringsElement: document.getElementById('typed-strings'),
+      typeSpeed: 50,
+      loop: true
+		})
 
 
 })
