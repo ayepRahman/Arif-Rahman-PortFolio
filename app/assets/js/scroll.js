6d82f1491912b7202c534db8842dd49a15769f3b
arif rahman <matrep88@hotmail.com>
2017-05-24 00:37:09 +0800

    'working-on-mywork'

--- app/assets/js/scroll.js
+++ app/assets/js/scroll.js
@@ -1,10 +1,10 @@
-$(window).scroll(function(){
+$(window).scroll(function() {
 
   var winScroll = $(this).scrollTop()
 
   // navbar animation
-  if(winScroll >  $('.navbar-fixed').offset().top){
-    console.log('scrolling');
+  if (winScroll > $('.navbar-fixed').offset().top) {
+
     $('nav').removeClass('grey lighten-4').addClass('transition-opacity')
   } else {
     $('nav').addClass('grey lighten-4').removeClass('transition-opacity')
@@ -13,37 +13,48 @@ $(window).scroll(function(){
   // about animation
   if (winScroll > $('.about-row').offset().top - ($(window).height() / 1.2)) {
 
-      console.log("about");
+    $('.about-row .about-col').each(function(i) {
 
-      $('.about-row .about-col').each(function(i) {
+      setTimeout(function() {
+        $('.about-row .about-col').eq(i).addClass('pop')
+      }, 150 * (i + 1))
 
-        setTimeout(function() {
-          $('.about-row .about-col').eq(i).addClass('pop')
-        }, 150 * (i + 1))
+    })
 
-      })
+  }
 
-    }
+  // about title
+  if (winScroll > $('.about-title').offset().top - ($(window).height() / 1.2)) {
 
-    // about title
-    if (winScroll > $('.about-title').offset().top - ($(window).height() / 1.2)) {
-        console.log("about here");
+    $('.about-title').addClass('pop')
 
-        $('.about-title').addClass('pop')
+  }
 
-      }
+  // skills title
+  if (winScroll > $('.skills-title').offset().top - ($(window).height() / 1.2)) {
 
-    // skills title
-    if (winScroll > $('.skills-title').offset().top - ($(window).height() / 1.2)) {
-        console.log("skills here");
+    $('.skills-title').addClass('pop')
 
-        $('.skills-title').addClass('pop')
+  }
 
-      }
+  // development animate
+  if (winScroll > $('.skills-col-d').offset().top - ($(window).height() / 1.2)) {
 
+    $('.skills-col-d').addClass('pop')
 
+  }
 
+  // tools animate
+  if (winScroll > $('.skills-col-t').offset().top - ($(window).height() / 1.2)) {
 
+    $('.skills-col-t').addClass('pop')
 
+  }
 
+  // canvas flip animate
+  if (winScroll > $('.canvas-container').offset().top - ($(window).height() / 1.2)) {
+
+    $('.canvas-container').addClass('flip')
+
+  }
 })
