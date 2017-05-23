15f5232b4abdc210747c893665b71425ac10e756
arif rahman <matrep88@hotmail.com>
2017-05-23 13:37:28 +0800

    'skills'

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,33 +1,28 @@
-$( document ).ready(function(){
+$(document).ready(function() {
 
   // parrallax effect
   $('.parallax').parallax();
 
   // banner animation
-  setTimeout(function(){
+  setTimeout(function() {
     $('.fly-in-text').removeClass('hidden')
   }, 300)
   // typed animation
   Typed.new('#typed', {
-			stringsElement: document.getElementById('typed-strings'),
-      typeSpeed: 50,
-      loop: true
-		})
+    stringsElement: document.getElementById('typed-strings'),
+    typeSpeed: 50,
+    loop: true
+  })
   // scrollspy - smooth scrolling to id
-  $('.scrollspy').scrollSpy({
-    scrollOffset:0
-  });
+  $('.scrollspy').scrollSpy({scrollOffset: 70});
 
   // responsive collapse/ fixed sideNav issue with link
-  $('.button-collapse').sideNav({
-    closeOnClick: true
-  })
+  $('.button-collapse').sideNav({closeOnClick: true})
 
   $('.button-collapse').click(removeOverlay);
 
   function removeOverlay() {
-  $('div[id^=sidenav-overlay]').remove();
-}
-
+    $('div[id^=sidenav-overlay]').remove();
+  }
 
 })
--- app/assets/js/scroll.js
+++ app/assets/js/scroll.js
@@ -3,10 +3,37 @@ $(window).scroll(function(){
   var winScroll = $(this).scrollTop()
 
   // navbar animation
-  if(winScroll > $('.navbar-fixed').offset().top){
+  if(winScroll >  $('.navbar-fixed').offset().top){
     console.log('scrolling');
     $('nav').removeClass('grey lighten-4').addClass('transition-opacity')
   } else {
     $('nav').addClass('grey lighten-4').removeClass('transition-opacity')
   }
+
+  // about animation
+  if (winScroll > $('.about-row').offset().top - ($(window).height() / 1.2)) {
+
+      console.log("about");
+
+      $('.about-row .about-col').each(function(i) {
+
+        setTimeout(function() {
+          $('.about-row .about-col').eq(i).addClass('pop')
+        }, 150 * (i + 1))
+
+      })
+
+    }
+
+    // skills title
+    if (winScroll > $('.skills-title').offset().top - ($(window).height() / 1.2)) {
+        console.log("skills here");
+
+        $('.skills-title').addClass('pop')
+
+      }
+
+
+
+
 })
