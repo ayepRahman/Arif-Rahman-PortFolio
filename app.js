f8af93bb401505a855bcead2c712826ec3470e70
arif rahman <matrep88@hotmail.com>
2017-05-26 22:19:33 +0800

    'port'

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const routes = require('./config/router')
 app.use('/', routes)
 
 // set port and listen
-app.set('port', (5000))
+app.set('port', (process.env.PORT || 5000))
 app.listen(app.get('port'), function() {
-    console.log('My PortFolio open on http://localhost:' + app.get('port'))
+    console.log('myportfolio http://localhost ' + app.get('port'))
 })
