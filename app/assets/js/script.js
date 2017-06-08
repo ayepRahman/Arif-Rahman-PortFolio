15a8e8cfb569a868a1d23c06808382b79d05af39
arif rahman <matrep88@hotmail.com>
2017-06-08 19:43:39 +0800

    'responsive-and-unseafescript'

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -14,14 +14,14 @@ $(document).ready(function() {
     typeSpeed: 50,
     loop: true
   })
-  
+
   // scrollspy - smooth scrolling to id
   $('.scrollspy').scrollSpy({scrollOffset: 70});
-
   // responsive collapse/ fixed sideNav issue with link
   $('.button-collapse').sideNav({closeOnClick: true})
 
   $('.button-collapse').click(removeOverlay);
+
 
   function removeOverlay() {
     $('div[id^=sidenav-overlay]').remove();
